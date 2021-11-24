42dda047c47c2c42e27e13edef607de184944d3e
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-24 15:29:18 +0530

    Added : Program for the take multiple ride and calculate the aggragat

--- src/test/java/com/bridgelabz/CabInviceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInviceGenerator/InvoiceServiceTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.Test;
 
 public class InvoiceServiceTest {
 	@Test
@@ -24,5 +23,13 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
 
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, fare, 0.0);
 	}
 }
