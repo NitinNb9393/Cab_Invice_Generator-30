122a6346aaa9afd1410f0712cdbbe580c18c723f
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-24 15:35:52 +0530

    Added : Program for the returns total rides, total fare and avg fare

--- src/main/java/com/bridgelabz/CabInviceGenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInviceGenerator/InvoiceGenerator.java
@@ -9,10 +9,7 @@ public class InvoiceGenerator {
 	// calculate the total fare
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-		if (totalFare < MINIMUM_FARE)
-			return MINIMUM_FARE;
-		else
-			return totalFare;
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
 
 	// calculating Fare for Multiple rides
@@ -22,5 +19,14 @@ public class InvoiceGenerator {
 			totalFare += this.calculateFare(ride.distance, ride.time);
 		}
 		return totalFare;
+	}
+
+	// calculating totalFare, average Fare for multiple rides
+	public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
 	}
 }
